484909373dc9be6082c3d55467d5ebd106a1fdbc
unknown <pennykoin@fam-base.net>
2019-03-01 07:13:37 -0500

    revert crypto

--- src/crypto/random.c
+++ src/crypto/random.c
@@ -1,5 +1,5 @@
-// Copyright (c) 2011-2016 The Cryptonote developers
-// Copyright (c) 2014-2016 SDN developers
+// Copyright (c) 2011-2017 The Cryptonote developers
+// Copyright (c) 2018 The Circle Foundation
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
