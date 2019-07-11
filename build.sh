16d0c22d82974f33c9c4a0b7beb5e900758667a8
Jerry Howell <39189962+Pennykoin@users.noreply.github.com>
2019-07-11 11:49:43 -0400

    Update build.sh
    was pulling from testing branch

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt update && sudo apt-get -y upgrade && sudo apt-get -y install screen make cmake build-essential libboost-all-dev pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libminiupnpc-dev libreadline6-dev libldns-dev git nano mc && git clone https://github.com/Pennykoin-Dev-Team/PKCli -b testing && cd PKCli && make
+sudo apt update && sudo apt-get -y upgrade && sudo apt-get -y install screen make cmake build-essential libboost-all-dev pkg-config libssl-dev libzmq3-dev libunbound-dev libsodium-dev libminiupnpc-dev libreadline6-dev libldns-dev git nano mc && git clone https://github.com/Pennykoin-Dev-Team/PKCli && cd PKCli && make
